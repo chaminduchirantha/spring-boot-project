142c5453ccbc80b626d1ecdda51f85482037e004
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-07-17 17:47:47 +0530

    Initial commit

---
+++ sms-backend/src/test/java/lk/ijse/gdse/smsbackend/SmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.smsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
